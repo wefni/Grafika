e000fec692cf559e1f617b1fc97ba59ea740a99a
David <halaszdavid10@gmail.com>
2023-03-31 00:28:09 +0200

    Javított hamik, mar jo

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -33,7 +33,7 @@
 //=============================================================================================
 #include "framework.h"
 
-// vertex shader in GLSL: It is a Raw string (C++11) since it contains new line characters
+
 const char * const vertexSource = R"(
 	#version 330				// Shader 3.3
 	precision highp float;		// normal floats, makes no difference on desktop computers
@@ -46,7 +46,7 @@ const char * const vertexSource = R"(
 	}
 )";
 
-// fragment shader in GLSL
+
 const char * const fragmentSource = R"(
 	#version 330			// Shader 3.3
 	precision highp float;	// normal floats, makes no difference on desktop computers
@@ -59,29 +59,52 @@ const char * const fragmentSource = R"(
 	}
 )";
 
-GPUProgram gpuProgram; // vertex and fragment shaders
-unsigned int vao;	   // virtual world on the GPU
+GPUProgram gpuProgram;
+unsigned int vao;
+unsigned int vbo[12];
 
 
-////                Írt függvények              /////////////
 
 float newDot(const vec3& v1, const vec3& v2) {
-    return (v1.x * v2.x + v1.y * v2.y - v1.z * v2.z);                                             // Skaláris szorzás
+    return (v1.x * v2.x + v1.y * v2.y - v1.z * v2.z);
+}
+
+vec3 norm(vec3 v) {
+    return v * (1 / sqrtf(newDot(v,v)));
+}
+
+float p_w(vec3 p){
+    return sqrtf((p.x*p.x+p.y*p.y+1));
+}
+
+vec3 hip_w(vec3 v,vec3 p){
+    vec3 vek(v.x,v.y,((v.x*p.x+v.y*p.y)/p_w(p)));
+    return vek;
 }
 
 vec3 mer(vec3 v,vec3 p){
-    return cross(vec3(v.x,v.y,-v.z),vec3(p.x,p.y,-p.z));         // Merőleges állítás
+    v = norm(v);
+    p.z = p_w(p);
+    v = hip_w(v,p);
+    return cross(vec3(v.x,v.y,-v.z),vec3(p.x,p.y,-p.z));
 }
 
-vec3 vel_vector(vec3 v,vec3 p,float t){                                                         // Sebesség vektor számítás
+vec3 vel_vector(vec3 v,vec3 p,float t){
+    v = norm(v);
+    p.z = p_w(p);
+    v = hip_w(v,p);
     return p*coshf(t)+v*sinhf(t);
 }
 
-vec3 t_dis(vec3 v,vec3 p,float t){                                                              // Pont helye t idővel később
+vec3 der(vec3 v,vec3 p,float t){
+    v = norm(v);
+    p.z = p_w(p);
+    v = hip_w(v,p);
     return p*sinhf(t)+v*coshf(t);
 }
 
-vec3 dir(vec3 p,vec3 q,float t){                                                                // Pont iránya
+vec3 dir(vec3 p,vec3 q,float t){
+
     return (q-p*coshf(t))/sinhf(t);
 }
 
@@ -89,165 +112,216 @@ float distance(vec3 p,vec3 q){                                                  
     return acoshf(newDot(p,q));
 }
 
-vec3 rotate(vec3 v,vec3 p,float phi){                                                           // Elforgatás fi szöggel
+
+vec3 rotate(vec3 v,vec3 p,float phi){
+    v = norm(v);
+    p.z = p_w(p);
+    v = hip_w(v,p);
     return v*cosf(phi) + mer(v,p)*sinf(phi);
 }
 
 vec3 pont(vec3 v,vec3 p){
-    float t = sqrtf(newDot(v,v));                                                       // Egy ponthoz képest adott irányban és távolságra lévő pont előállítása
+    v = norm(v);
+    p.z = p_w(p);
+    v = hip_w(v,p);
+    float t = sqrtf(newDot(v,v));
     return p*coshf(t)+v*sinhf(t);
 }
 
-vec3 norm(vec3 v) {
-    return v * (1 / sqrtf(newDot(v,v)));
-}
-
-float p_w(vec3 p){                                                                                // Pontok
-    return sqrtf((p.x*p.x+p.y*p.y+1));
-}
-
-vec3 hip_w(vec3 v,vec3 p){
-    vec3 vek(v.x,v.y,((v.x*p.x+v.y*p.y)/p_w(p)));
-    return vek;                                                              // Merőleges pont visszavetítés
-}
-
 const int nv = 1000;
-vec3 vert[nv];
 
-unsigned int vbo;
-unsigned int vbo2;
 
 bool pressed[256] = {false,};
 float yRot = 0.0f;
 float xRot = 0.0f;
 
+vec3 p1;
+
+class Hami{
+public:
+    vec3 origo;
+    vec3 vektor;
+    unsigned int id;
+    unsigned int id2;
+
+
+    Hami(vec3 o,vec3 v,unsigned int i,unsigned int j){
+        origo = o;
+        vektor = v;
+        id = i;
+        id2 = j;
+    }
+
+    void drawHami(){
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+        glGenBuffers(1, &vbo[id]);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo[id]);
+
+        vektor = norm(vektor);
+        origo.z = p_w(origo);
+        vektor = hip_w(vektor,origo);
+
+        vec3 vertices[nv];
+        vec3 eP;
+        vec3 eP2;
+
+
+        for(int i = 0; i < nv; ++i) {
+            p1 = vel_vector(vektor,origo,0.35);
+            p1.z = p_w(p1);
+            vertices[i] = p1;
+
+           if(i==40) eP = p1;
+           if(i==240) eP2 = p1;
+
+            vektor = rotate(vektor,origo,0.00628318531);
+        }
+
+        glBufferData(GL_ARRAY_BUFFER,sizeof(vec3)*nv,vertices,GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE,0, NULL);
+
+        drawSzem(eP,id2);
+        drawSzem(eP2,id2+1);
+    }
+
+    void drawSzem(vec3 ep,int iD){
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        glGenBuffers(1, &vbo[iD]);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo[iD]);
+
+        vec3 t(ep.x,ep.y,ep.z);
+        vec3 vek(1,1,0);
+        vek = norm(vek);
+        t.z = p_w(t);
+        vek = hip_w(vek,t);
+
+        vec3 vertices[nv];
+
+        for(int i = 0; i < nv; ++i) {
+            vec3 p2 = vel_vector(vek,t,0.1f);
+            p2.z = p_w(p2);
+            vertices[i] = p2;
+            vek = rotate(vek,t,0.00628318531);
+        }
+
+        glBufferData(GL_ARRAY_BUFFER,
+                     sizeof(vec3)*nv,
+                     vertices,
+                     GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0,
+                              3, GL_FLOAT, GL_FALSE,
+                              0, NULL);
+    }
+
+    void setX(float ex){
+        origo.x = ex;
+    }
+    void setY(float eY){
+        origo.y = eY;
+    }
+};
+
+
 
 void circ(){
 
-    glGenVertexArrays(1, &vao);	// get 1 vao id
+    glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
 
-    glGenBuffers(1, &vbo);	// Generate 1 buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glGenBuffers(1, &vbo[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 
-    vec3 kor(0,0,1);
-    vec3 nagyVek(1.54,0,0);
+    vec3 vert[nv];
 
     for (int i = 0; i < nv; ++i) {
-        vec3 p1 = pont(nagyVek,kor);
-        vert[i] = p1;
-        nagyVek = rotate(nagyVek,kor,0.00628318531);
+       float fi = i*2*M_PI/nv;
+       vert[i] = vec3(cosf(fi),sinf(fi),0);
     }
 
 
-    glBufferData(GL_ARRAY_BUFFER, 	// Copy to GPU target
-                 sizeof(vec3)*nv,  // # bytes
-                 vert,	      	// address
-                 GL_STATIC_DRAW);	// we do not change later
+    glBufferData(GL_ARRAY_BUFFER,
+                 sizeof(vec3)*nv,
+                 vert,
+                 GL_STATIC_DRAW);
 
 
-    glEnableVertexAttribArray(0);  // AttribArray 0
-    glVertexAttribPointer(0,       // vbo -> AttribArray 0
-                          3, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
-                          0, NULL); 		     // stride, offset: tightly packed
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,
+                          3, GL_FLOAT, GL_FALSE,
+                          0, NULL);
 
 
 }
 
-void hip_circ(float yrot,float xrot){
-
-    glGenVertexArrays(1, &vao);	// get 1 vao id
-    glBindVertexArray(vao);
-
-    glGenBuffers(1, &vbo2);	// Generate 1 buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-
-    vec3 ori(xrot,yrot,1);
-    vec3 vek(1,0,0);
-    ori.z = p_w(ori);
-    vek = hip_w(vek,ori);
-    vek = norm(vek);
-    vec3 vertices[nv];
-
-
-    for(int i = 0; i < nv; ++i) {
-      vec3 p1 = pont(vek,ori);
-      p1.z = p_w(p1);
-      vertices[i] = p1;
-      vek = rotate(vek,ori,0.0628318531);
-    }
-
-
-    glBufferData(GL_ARRAY_BUFFER, 	// Copy to GPU target
-                 sizeof(vec3)*nv,  // # bytes
-                 vertices,	      	// address
-                 GL_STATIC_DRAW);	// we do not change later
-
-    glEnableVertexAttribArray(0);  // AttribArray 0
-    glVertexAttribPointer(0,       // vbo -> AttribArray 0
-                          3, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
-                          0, NULL); 		     // stride, offset: tightly packed
-
-}
-
-///////////////////////////////////////////////////////////////
-
-
-
-
-// Initialization, create an OpenGL context
 void onInitialization() {
     glViewport(0, 0, windowWidth, windowHeight);
-
     circ();
 
-
-
-    // create program for the GPU
     gpuProgram.create(vertexSource, fragmentSource, "outColor");
 }
 
-// Window has become invalid: Redraw
+Hami h(vec3(0,0,1.0),vec3(1,1,0.0),1,2);
+
 void onDisplay() {
-    glClearColor(0.25, 0.25, 0.25, 0);     // background color
-    glClear(GL_COLOR_BUFFER_BIT); // clear frame buffer
+    glClearColor(0.25, 0.25, 0.25, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     int location = glGetUniformLocation(gpuProgram.getId(), "color");
-    glUniform3f(location, 0.0f, 0.0f, 0.0f); // 3 floats
+    glUniform3f(location, 0.0f, 0.0f, 0.0f);
 
-    //const float r0 = xmax;
-    hip_circ(yRot,xRot);
+    h.drawHami();
 
-    glBindVertexArray(vao);  // Draw call
+    glBindVertexArray(vao);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE,0, NULL);
-    glDrawArrays(GL_TRIANGLE_FAN, 0/*startIdx*/, nv/*# Elements*/);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, nv);
 
 
     int location2 = glGetUniformLocation(gpuProgram.getId(), "color");
-    glUniform3f(location2, 1.0f, 0.0f, 0.0f); // 3 floats
+    glUniform3f(location2, 1.0f, 0.0f, 0.0f);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
     glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE,0, NULL);
-    glDrawArrays(GL_TRIANGLE_FAN, 0/*startIdx*/, nv/*# Elements*/);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, nv);
+
+    int location3 = glGetUniformLocation(gpuProgram.getId(), "color");
+    glUniform3f(location3, 1.0f, 1.0f, 1.0f);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+    glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE,0, NULL);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, nv);
+
+    int location4 = glGetUniformLocation(gpuProgram.getId(), "color");
+    glUniform3f(location4, 1.0f, 1.0f, 1.0f);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
+    glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE,0, NULL);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, nv);
 
 
-    glutSwapBuffers(); // exchange buffers for double buffering
+    glutSwapBuffers();
 }
 
-// Key of ASCII code pressed
+
 void onKeyboard(unsigned char key, int pX, int pY) {
    pressed[key] = true;
 }
 
-// Key of ASCII code released
+
 void onKeyboardUp(unsigned char key, int pX, int pY) {
     pressed[key] = false;
 }
 
-// Move mouse with key pressed
+
 void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
     // Convert to normalized device space
     float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
@@ -255,7 +329,7 @@ void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the 
     printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
 }
 
-// Mouse click event
+
 void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
     // Convert to normalized device space
     float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
@@ -274,16 +348,15 @@ void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel co
     }
 }
 
-// Idle event indicating that some time elapsed: do animation here
+
 void onIdle() {
     float time = glutGet(GLUT_ELAPSED_TIME)/1000.0f;
 
-    if(pressed['e']){
-        yRot += 0.01f*time;
-    }else if(pressed['s']){
-        xRot += 0.01f*time;
-    }else if(pressed['f']){
-        xRot -= 0.01f*time;
+    if(pressed['e']) {
+           h.origo.y += 0.001f*time;
+    }
+    if(pressed['d']) {
+        h.setY(-0.5f*time);
     }
 
 
